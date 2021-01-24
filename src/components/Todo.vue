907ff268da611716b860f1fba6556160b22bc416
RecursivePerson <possawat2017@hotmail.com>
2021-01-25 02:24:45 +0700

    working on update

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,74 +1,107 @@
 <template>
-<div>
+  <div>
     <section class="todoapp">
-        <header class="header">
-            <h1>{{ title }}</h1>
-            <input class="new-todo" placeholder="What needs to be done?" autofocus>
-        </header>
+      <header class="header">
+        <h1>{{ title }}</h1>
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?"
+          v-on:keyup.enter="createTodo"
+          autofocus
+        />
+      </header>
 
-        <!-- This section should be hidden by default and shown when there are todos -->
-        <section class="main">
-            <input id="toggle-all" class="toggle-all" type="checkbox">
-            <label for="toggle-all">Mark all as complete</label>
-            <ul class="todo-list">
-                <!-- These are here just to show the structure of the list items -->
-                <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-                <li class="completed">
-                    <div class="view">
-                        <input class="toggle" type="checkbox" checked>
-                        <label>Taste JavaScript</label>
-                        <button class="destroy"></button>
-                    </div>
-                    <input class="edit" value="Create a TodoMVC template">
-                </li>
-                <li>
-                    <div class="view">
-                        <input class="toggle" type="checkbox">
-                        <label>Buy a unicorn</label>
-                        <button class="destroy"></button>
-                    </div>
-                    <input class="edit" value="Rule the web">
-                </li>
-            </ul>
-        </section>
-        <!-- This footer should hidden by default and shown when there are todos -->
-        <footer class="footer">
-            <!-- This should be `0 items left` by default -->
-            <span class="todo-count"><strong>0</strong> item left</span>
-            <!-- Remove this if you don't implement routing -->
-            <ul class="filters">
-                <li>
-                    <a class="selected" href="#/">All</a>
-                </li>
-                <li>
-                    <a href="#/active">Active</a>
-                </li>
-                <li>
-                    <a href="#/completed">Completed</a>
-                </li>
-            </ul>
-            <!-- Hidden if no completed items are left ↓ -->
-            <button class="clear-completed">Clear completed</button>
-        </footer>
+      <!-- This section should be hidden by default and shown when there are todos -->
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <!-- These are here just to show the structure of the list items -->
+          <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
+          <li
+            v-for="todo in todos"
+            :key="todo.text"
+            :class="{ completed: todo.isDone, editing: todo === editing }"
+          >
+            <div class="view">
+              <input class="toggle" type="checkbox" v-model="todo.isDone" />
+              <label @dblclick="startEditing(todo)">{{ todo.text }}</label>
+              <button class="destroy"></button>
+            </div>
+            <input
+              class="edit"
+              @keyup.esc="cancelEditing"
+              @keyup.enter="finishEditing"
+              @blur="finishEditing"
+              :value="todo.text"
+            />
+          </li>
+        </ul>
+      </section>
+      <!-- This footer should hidden by default and shown when there are todos -->
+      <footer class="footer">
+        <!-- This should be `0 items left` by default -->
+        <span class="todo-count"><strong>0</strong> item left</span>
+        <!-- Remove this if you don't implement routing -->
+        <ul class="filters">
+          <li>
+            <a class="selected" href="#/">All</a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <!-- Hidden if no completed items are left ↓ -->
+        <button class="clear-completed">Clear completed</button>
+      </footer>
     </section>
     <footer class="info">
-        <p>Double-click to edit a todo</p>
-        <!-- Remove the below line ↓ -->
-        <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-        <!-- Change this out with your name and url ↓ -->
-        <p>Created by <a href="http://todomvc.com">you</a></p>
-        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      <p>Double-click to edit a todo</p>
+      <!-- Remove the below line ↓ -->
+      <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+      <!-- Change this out with your name and url ↓ -->
+      <p>Created by <a href="http://todomvc.com">you</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
     </footer>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'todo',
+  name: "todo",
   data() {
-      return {
-        title: 'Hello!',
-      }
+    return {
+      title: "Easy Todo",
+      todos: [
+        { text: "Learn JavaScript ES6+ goodies", isDone: true },
+        { text: "Learn Vue", isDone: false },
+        { text: "Build something awesome", isDone: false }
+      ]
+    };
   },
-}
+  methods: {
+    createTodo(event) {
+      const textbox = event.target;
+      this.todos.push({ text: textbox.value, isDone: false });
+      textbox.value = "";
+    },
+    startEditing(todo) {
+      this.editing = todo;
+    },
+    finishEditing(event) {
+      if (!this.editing) {
+        return;
+      }
+      const textbox = event.target;
+      this.editing.text = textbox.value;
+      this.editing = null;
+    },
+    cancelEditing() {
+      this.editing = null;
+    }
+  }
+};
 </script>
